f2e9ef7a9914b314976a10fb4ad7a5078f9e2329
Efflam Castel <efflamm.castel@gmail.com>
2023-01-09 21:23:34 +0000

    minimal login & user management system

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def sign_in
+    if session[:user_id]
+      redirect_to games_path
+    end
+
+    @users = User.all
+    @user = User.new
+  end
+
+  def sign_out
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  def log_in
+    @user = User.find(params[:existing_user][:user_id])
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  
+  get '/training', to: 'training#show'
+  post '/sign_out', as: :sign_out, to: 'users#sign_out'
+  post '/users/log_in', to: 'users#log_in'
+  resources :users, only: [:create] do
+  end
+
   resources :games do
     post :import, on: :collection
   end
-  root "games#index"
+  root "users#sign_in"
   
 end
