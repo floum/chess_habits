aa772b4151b946f8275a0b3f1b12e032c63f3ec9
Efflam Castel <efflamm.castel@gmail.com>
2023-01-10 22:28:29 +0000

    legal moves chessboard

--- app/javascript/chessboard.js
+++ app/javascript/chessboard.js
@@ -1,8 +1,24 @@
 import { Chessground } from 'chessground';
+import { Chess } from 'chess.js';
 
 export class Chessboard {
   constructor(element) {
+    this.config = {
+      movable: {
+	events: {
+	  after: (src, dest, metadata) => {
+	    this.game.move({ from: src, to: dest});
+            this.board.set({fen: this.game.fen()});
+          }
+        }
+      }
+    }
     this.element = element;
-    this.board = Chessground(this.element, {movable: true});
+    this.board = Chessground(this.element, this.config);
+    this.game = new Chess();
+  }
+  
+  currentFen() {
+    this.game.fen();
   }
 }
