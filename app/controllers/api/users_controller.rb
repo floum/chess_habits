d5e86021f33a48763e2d13b7fec5e867d4e0ee1d
efflam castel <efflam.castel@orange.com>
2023-01-24 11:29:22 +0100

    create user if it does not exist on /users/:name

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,16 @@ module Api
   class UsersController < ActionController::API
     def show
       @user = User.find_by(name: params[:name])
+      unless @user
+        @user = User.create(user_params)
+      end
       render json: @user
+    end
+
+    private
+
+    def user_params
+      params.permit(:name)
     end
   end
 end
